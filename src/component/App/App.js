89ad44014dda1f89aab4718fb994c7280b8c2113
Jonathan Martel <jonathmartel@gmail.com>
2022-04-26 17:05:08 -0400

    avec un composant stateful

--- src/component/App/App.js
+++ src/component/App/App.js
@@ -6,18 +6,49 @@ import Entete from '../Entete/Entete';
 export default class App extends React.Component{
   constructor(){
     super();
+    this.state = {  // État
+      compteur : 0  
+    };
+
+    this.augmenter = this.augmenter.bind(this);
+    this.decroitre = this.decroitre.bind(this);
   }
 
+  augmenter(){
+    console.log("+1");
+    //this.state.compteur++;
+    this.setState(  {
+                      compteur : this.state.compteur+1
+                    }
+                  );
+    console.log(this.state.compteur);
+  }
+
+  decroitre(){
+    console.log("-1");
+    //this.state.compteur++;
+    this.setState({
+        compteur : this.state.compteur - 1
+    })
+    //this.props.nombre--;
+
+
+  }
   render(){
 
     // Il peut y avoir du code ici...
 
     return(
       <main>
-        <Entete />
+        <Entete titre="Mon application react" fctDecroitre={this.decroitre} nombre={this.state.compteur}/>
+        <Entete titre="Mon application react" fctDecroitre={this.decroitre} nombre={this.state.compteur}/>
 
         <article>
           <h1>Allo le monde</h1>
+          <p>Nombre de truc</p>
+          <p>{this.state.compteur}</p>
+          <button onClick={this.augmenter}>Augmenter les trucs</button>
+
         </article>
       </main>
       
--- src/component/Entete/Entete.js
+++ src/component/Entete/Entete.js
@@ -2,17 +2,36 @@ import './Entete.css';
 import React from 'react';
 
 export default class Entete extends React.Component{
-  constructor(){
-    super();
+  constructor(props){
+    super(props);
+
+    this.state = {compteur : props.nombre};
+
+    this.moins = this.moins.bind(this)
+    this.plus = this.plus.bind(this)
   }
 
-  render(){
+  plus(){
+      this.setState({
+          compteur : this.state.compteur + 1
+      });
+  }
 
+  moins(){
+    this.setState({
+        compteur : this.state.compteur - 1
+    });
+}
+  render(){
+    const titre = this.props.titre || "Titre de l'application";
     // Il peut y avoir du code ici...
 
     return(
       <header>
-        <h1>Ma première démo react</h1>
+        <h1>{titre}</h1>
+        <p>Compteur : {this.state.compteur}</p> <button onClick={this.moins}>moins</button><button onClick={this.plus}>plus</button>
+        <p>Nombre : {this.props.nombre}</p>
+        <button onClick={this.props.fctDecroitre}>Décroître les trucs</button>
       </header>
     );
   }
