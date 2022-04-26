1d47370db96349804369c4db357709710a9d05ee
Jonathan Martel <jonathmartel@gmail.com>
2022-04-26 16:06:55 -0400

    avec readme.md

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './component/App/App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// JSX
 root.render(
   <React.StrictMode>
     <App />
